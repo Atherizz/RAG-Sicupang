27015e0e230e54de2107fa8f4fb9234c8b45811b
Atherizz <saveroathallah86@gmail.com>
2025-10-08 23:12:54 +0700

    balik setingan awal

--- app/db/database.py
+++ app/db/database.py
@@ -11,27 +11,19 @@ class DBService:
         self.DB_USER = os.getenv("DB_USER")
         self.DB_PASSWORD = os.getenv("DB_PASSWORD")
         self.DB_NAME = os.getenv("DB_NAME")
-        self.DB_INSTANCE_CONN_NAME = os.getenv("DB_INSTANCE_CONN_NAME")
-        
-        if self.DB_INSTANCE_CONN_NAME:
-            self.mysql_url = (
-                f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}"
-                f"@/{self.DB_NAME}?unix_socket=/cloudsql/{self.DB_INSTANCE_CONN_NAME}"
-            )
-        else:
-            self.DB_HOST = os.getenv("DB_HOST")
-            self.DB_PORT = os.getenv("DB_PORT")
-            self.mysql_url = f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-        
+        self.DB_HOST = os.getenv("DB_HOST")
+        self.DB_PORT = os.getenv("DB_PORT")
+        self.mysql_url = f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         self.engine = create_engine(self.mysql_url, echo=False)
         
     def get_sql_database(self):
         db = SQLDatabase.from_uri(self.mysql_url)
         return db
-        
+    
     def get_session(self):
         with Session(self.engine) as session:
             yield session
             
     def create_db_and_tables(self):
         SQLModel.metadata.create_all(self.engine)
+        
